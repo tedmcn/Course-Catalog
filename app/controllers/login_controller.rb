29eb3b89d37ae0076404c3f47b2c9ec180fe67ab
Ted McNulty <this@brandeis.edu>
2016-03-21 23:42:53 -0400

    heroku?

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,10 +2,8 @@ class LoginController < ApplicationController
   def welcome
     @my_enrollements = Enrollment.where("user_id = ?", current_user.id)
     @my_courses=[];
-    if @my_enrollements != []
-      @my_enrollements.each do |c|
-        @my_courses.push(Course.find(c.course_id))
-      end
+    @my_enrollements.each do |c|
+      @my_courses.push(Course.find(c.course_id))
     end
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+
+  config.consider_all_requests_local       = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.

eaa688cc26d8980beb986962faa62ccc29a2d39c
Ted McNulty <this@brandeis.edu>
2016-03-21 23:33:33 -0400

    herokuuuuu

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,8 +2,10 @@ class LoginController < ApplicationController
   def welcome
     @my_enrollements = Enrollment.where("user_id = ?", current_user.id)
     @my_courses=[];
-    @my_enrollements.each do |c|
-      @my_courses.push(Course.find(c.course_id))
+    if @my_enrollements != []
+      @my_enrollements.each do |c|
+        @my_courses.push(Course.find(c.course_id))
+      end
     end
   end
 end

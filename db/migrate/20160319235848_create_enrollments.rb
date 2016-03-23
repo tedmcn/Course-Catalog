b99a563439f4cd6021ace9a6612509267cf7cf60
Ted McNulty <this@brandeis.edu>
2016-03-23 19:42:09 -0400

    change schema string>int

--- db/migrate/20160319235848_create_enrollments.rb
+++ db/migrate/20160319235848_create_enrollments.rb
@@ -1,8 +1,8 @@
 class CreateEnrollments < ActiveRecord::Migration
   def change
     create_table :enrollments do |t|
-      t.string :user_id
-      t.string :course_id
+      t.integer :user_id
+      t.integer :course_id
       t.timestamps null: false
     end
   end

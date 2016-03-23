b7aa7421a20170599fe879f7e7ef5e4ae7717377
Ted McNulty <this@brandeis.edu>
2016-03-23 19:38:49 -0400

    check for sign in before tryng to get id in controller

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,10 +1,11 @@
 class LoginController < ApplicationController
-  include SessionsHelper
   def welcome
-    @my_enrollements = Enrollment.where("user_id = ?", current_user.id)
     @my_courses=[];
-    @my_enrollements.each do |c|
-      @my_courses.push(Course.find(c.course_id))
+    if signed_in?
+      @my_enrollements = Enrollment.where("user_id = ?", current_user.id)
+      @my_enrollements.each do |c|
+        @my_courses.push(Course.find(c.course_id))
+      end
     end
   end
 end

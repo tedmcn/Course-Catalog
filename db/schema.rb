8d0f0a367144de2b4cd322a93be47ee7bb47f5f9
Ted McNulty <this@brandeis.edu>
2016-03-23 18:37:08 -0400

    fixing

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20160321074822) do
   end
 
   create_table "enrollments", force: :cascade do |t|
-    t.string   "user_id"
+    t.integer   "user_id"
     t.string   "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
